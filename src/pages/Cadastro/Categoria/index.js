5b437b3cbd2ce09f108dfacd6ee85989675916b4
Sergio Ricardo <sergio-ricardo@hotmail.com>
2020-07-31 00:17:35 -0300

    Correção na url para fetch

--- src/pages/Cadastro/Categoria/index.js
+++ src/pages/Cadastro/Categoria/index.js
@@ -39,7 +39,9 @@ function CadastroCategoria() {
   };
 
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias';
+    const URL = window.location.href.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://sergioflix.herokuapp.com/categorias';
     fetch(URL)
       .then(async (responseFromServer) => {
         if (responseFromServer.ok) {
